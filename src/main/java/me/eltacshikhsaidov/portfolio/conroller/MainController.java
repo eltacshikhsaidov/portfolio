7ae6c54824a25d336da42f011d405a9f6604e29a
Eltac <56788221+eltacshikhsaidov@users.noreply.github.com>
2021-02-11 12:27:59 +0400

    Update MainController.java

--- src/main/java/me/eltacshikhsaidov/portfolio/conroller/MainController.java
+++ src/main/java/me/eltacshikhsaidov/portfolio/conroller/MainController.java
@@ -2,21 +2,12 @@ package me.eltacshikhsaidov.portfolio.conroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
 
 @Controller
 public class MainController {
-    
-    
-    public static syncronized int count() {
-        int count = 0;
-        count ++;
-        return count;
-    }
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("count", count());
+    public String index() {
         return "index";
     }
 
